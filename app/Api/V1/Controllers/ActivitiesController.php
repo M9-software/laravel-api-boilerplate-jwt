ede1074baa08a66c957bc4f4677e8dbf376bfae2
sun.xianwei <sun.xianwei@xcar.com.cn>
2020-02-01 07:45:30 +0800

    controller

---
+++ app/Api/V1/Controllers/ActivitiesController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Api\V1\Controllers;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Tymon\JWTAuth\JWTAuth;
+use App\Http\Controllers\Controller;
+use App\Api\V1\Requests\LoginRequest;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Auth;
+
+class ActivitiesController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function add()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,41 @@ $api->version('v1', function (Router $api) {
         $api->post('logout', 'App\\Api\\V1\\Controllers\\LogoutController@logout');
         $api->post('refresh', 'App\\Api\\V1\\Controllers\\RefreshController@refresh');
         $api->get('me', 'App\\Api\\V1\\Controllers\\UserController@me');
+
+        /*
+        // 商圈 Areas
+        $api->get('areas', 'App\\Api\\V1\\Controllers\\AreasController@show');
+        $api->post('addarea', 'App\\Api\\V1\\Controllers\\AreasController@add');
+        $api->post('updatearea', 'App\\Api\\V1\\Controllers\\AreasController@update');
+
+        // 商户 Consumers
+        $api->get('consumers', 'App\\Api\\V1\\Controllers\\ConsumersController@show');
+        $api->post('addconsumer', 'App\\Api\\V1\\Controllers\\ConsumersController@add');
+        $api->post('updateconsumer', 'App\\Api\\V1\\Controllers\\ConsumersController@update');
+
+        // 活动 Activities
+        $api->get('activities', 'App\\Api\\V1\\Controllers\\ActivitiesController@show');
+        $api->post('addactivity', 'App\\Api\\V1\\Controllers\\ActivitiesController@add');
+        $api->post('updateactivity', 'App\\Api\\V1\\Controllers\\ActivitiesController@update');
+
+        // 联系人-客户 Contacts
+        $api->get('contacts', 'App\\Api\\V1\\Controllers\\ContactsController@show');
+        $api->post('addcontact', 'App\\Api\\V1\\Controllers\\ContactsController@add');
+
+        // 预约 Books
+        $api->get('books', 'App\\Api\\V1\\Controllers\\BooksController@show');
+        $api->post('addbook', 'App\\Api\\V1\\Controllers\\BooksController@add');
+
+        // 联系人-客户 地址 Contactaddrs
+        $api->get('contactaddrs', 'App\\Api\\V1\\Controllers\\ContactaddrsController@show');
+        $api->post('addcontactaddr', 'App\\Api\\V1\\Controllers\\ContactaddrsController@add');
+        $api->post('updatecontactaddr', 'App\\Api\\V1\\Controllers\\ContactaddrsController@update');
+
+        // 联系人-客户 信息 Contactinfos
+        $api->get('contactinfos', 'App\\Api\\V1\\Controllers\\ContactinfosController@show');
+        $api->post('addcontactinfo', 'App\\Api\\V1\\Controllers\\ContactinfosController@add');
+        $api->post('updatecontactinfo', 'App\\Api\\V1\\Controllers\\ContactinfosController@update');
+        */
     });
 
     $api->group(['middleware' => 'jwt.auth'], function(Router $api) {
@@ -40,4 +75,5 @@ $api->version('v1', function (Router $api) {
             'message' => 'This is a simple example of item returned by your APIs. Everyone can see it.'
         ]);
     });
+
 });
